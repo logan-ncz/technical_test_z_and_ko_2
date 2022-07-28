59cb6dda76fc89be3596be96fa8ee401b99b226a
Logan Nochez <logannochez@hotmail.fr>
2022-07-28 16:04:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,175 @@
+var tables = document.querySelectorAll("table.sortable"),
+  table,
+  thead,
+  headers,
+  i,
+  j;
+
+for (i = 0; i < tables.length; i++) {
+  table = tables[i];
+
+  if ((thead = table.querySelector("thead"))) {
+    headers = thead.querySelectorAll("th");
+
+    for (j = 0; j < headers.length; j++) {
+      headers[j].innerHTML = "<a href='#'>" + headers[j].innerText + "</a>";
+    }
+
+    if (!(thead = table.querySelector("thead")).classList.contains("reverse")) {
+      thead.addEventListener("click", sortTableFunction(table));
+      thead.classList.add("reverse");
+    } else {
+      thead.addEventListener("click", sortTableFunctionReverse(table));
+      thead.classList.remove("reverse");
+    }
+  }
+}
+
+/**
+ * Create a function to sort the given table.
+ */
+function sortTableFunction(table) {
+  return function (ev) {
+    if (ev.target.tagName.toLowerCase() == "a") {
+      sortRows(table, siblingIndex(ev.target.parentNode));
+      ev.preventDefault();
+    }
+  };
+}
+
+function sortTableFunctionReverse(table) {
+  return function (ev) {
+    if (ev.target.tagName.toLowerCase() == "a") {
+      sortRows(table, siblingIndex(ev.target.parentNode));
+      ev.preventDefault();
+    }
+  };
+}
+
+/**
+ * Get the index of a node relative to its siblings — the first (eldest) sibling
+ * has index 0, the next index 1, etc.
+ */
+function siblingIndex(node) {
+  var count = 0;
+
+  while ((node = node.previousElementSibling)) {
+    count++;
+  }
+
+  return count;
+}
+
+/**
+ * Sort the given table by the numbered column (0 is the first column, etc.)
+ */
+function sortRows(table, columnIndex) {
+  var rows = table.querySelectorAll("tbody tr"),
+    sel = "thead th:nth-child(" + (columnIndex + 1) + ")",
+    sel2 = "td:nth-child(" + (columnIndex + 1) + ")",
+    classList = table.querySelector(sel).classList,
+    values = [],
+    cls = "",
+    allNum = true,
+    val,
+    index,
+    node;
+
+  if (classList) {
+    if (classList.contains("date")) {
+      cls = "date";
+    } else if (classList.contains("number")) {
+      cls = "number";
+    }
+  }
+
+  for (index = 0; index < rows.length; index++) {
+    node = rows[index].querySelector(sel2);
+    val = node.innerText;
+
+    if (isNaN(val)) {
+      allNum = false;
+    } else {
+      val = parseFloat(val);
+    }
+
+    values.push({ value: val, row: rows[index] });
+  }
+
+  if (cls == "" && allNum) {
+    cls = "number";
+  }
+
+  if (cls == "number") {
+    values.sort(sortNumberVal);
+  } else if (cls == "date") {
+    values.sort(sortDateVal);
+  } else {
+    values.sort(sortTextVal);
+  }
+
+  for (var idx = 0; idx < values.length; idx++) {
+    table.querySelector("tbody").appendChild(values[idx].row);
+  }
+}
+
+/**
+ * Compare two 'value objects' numerically
+ */
+function sortNumberVal(a, b) {
+  return sortNumber(a.value, b.value);
+}
+
+/**
+ * Numeric sort comparison
+ */
+function sortNumber(a, b) {
+  return a - b;
+}
+
+/**
+ * Compare two 'value objects' as dates
+ */
+function sortDateVal(a, b) {
+  var dateA = Date.parse(a.value),
+    dateB = Date.parse(b.value);
+
+  return sortNumber(dateA, dateB);
+}
+
+/**
+ * Compare two 'value objects' as simple text; case-insensitive
+ */
+function sortTextVal(a, b) {
+  var textA = (a.value + "").toUpperCase();
+  var textB = (b.value + "").toUpperCase();
+
+  if (textA < textB) {
+    return -1;
+  }
+
+  if (textA > textB) {
+    return 1;
+  }
+
+  return 0;
+}
+
+function searchFunction() {
+  // Declare variables
+  var input, filter, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  tr = Array.from(document.querySelectorAll("tr"));
+  tr.splice(0, 1);
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    txtValue = tr[i].textContent;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
